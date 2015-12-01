459de235c7a5eb54ef8b450d51f16b1803bca9c8
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2015-12-01 10:20:45 +0100

    Add NBA.java with databse credentials.

---
+++ src/NBA.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jaspar.mang on 01.12.15.
+ */
+public class NBA {
+    public static String url = "jdbc:postgresql://localhost:5432/testdb";
+    public static String user = "root";
+    public static String password = "root%";
+    public static String serverUrl = "jdbc:postgresql://sebastiankoall.de/infointe";
+    public static String serverUser = "infointe";
+    public static String serverPassword = "InfoInte1516%";
+
+}
