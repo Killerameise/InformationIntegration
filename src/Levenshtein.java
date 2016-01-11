2c7394ccb53b1e34021c763762be55b336ef5299
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2016-01-11 15:43:15 +0100

    some layout shit

--- src/Levenshtein.java
+++ src/Levenshtein.java
@@ -1,20 +1,20 @@
 public class Levenshtein {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         String a = "uniformiert";
         String b = "uninformiert";
         try {
-            System.out.println("distance(" + a + ", " + b + ") = " + distance(a,b));
+            System.out.println("distance(" + a + ", " + b + ") = " + distance(a, b));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     public static int distance(String a, String b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
-        
-        int[] costs = new int [b.length() + 1];
+
+        int[] costs = new int[b.length() + 1];
         for (int j = 0; j < costs.length; j++)
             costs[j] = j;
         for (int i = 1; i <= a.length(); i++) {
@@ -23,7 +23,7 @@ public class Levenshtein {
             for (int j = 1; j <= b.length(); j++) {
                 int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
                 nw = costs[j];
-                costs[j] = cj;                
+                costs[j] = cj;
             }
         }
         return costs[b.length()];
