24ca81c85b2f7d2b58ee01f597f51e0fa3a9732f
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2016-01-14 14:32:10 +0100

    ultra bug fix

--- src/Duplicates.java
+++ src/Duplicates.java
@@ -40,11 +40,17 @@ public class Duplicates {
             if (rsEntry1.next() && rsEntry2.next()) {
                 String[] mergedEntry = compare.compareResultSets(rsEntry1, rsEntry2);
                 //TODO: autogenerate the SQL with ResultSetMetaData
-                String insertSql = "INSERT INTO sportsman (firstname, lastname, birthdate, captain, position, \"Anzahl Einsaetze\", club_id, playernumber, country, games, goals, assist, hand, contract_until, market_value, belt, coach, familynamelocal, middlenamelocal, favoritetechnique, gender, middlename, shortname, side, \"birthCountry\", \"birthState\", \"birthCity\", \"deathYear\", \"deathMonth\", \"deathDay\", \"deathCountry\", \"deathState\", \"deathCity\", \"nameGiven\", weight, height, bats, throws, debut, \"finalGame\", \"retroID\", \"bbrefID\", comment, startdate, enddate, \"year\") " +
-                        "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+                String insertSql = "INSERT INTO sportsman (firstname, lastname, birthdate, captain, \"position\", " +
+                        "\"Anzahl Einsaetze\", club_id, playernumber, country, games, goals, assist, hand, contract_until, " +
+                        "market_value, belt, coach, familynamelocal, middlenamelocal, playername, firstnamelocal, favoritetechnique, " +
+                        "gender, middlename, " +
+                        "shortname, side, \"birthCountry\", \"birthState\", \"birthCity\", \"deathYear\", \"deathMonth\", " +
+                        "\"deathDay\", \"deathCountry\", \"deathState\", \"deathCity\", \"nameGiven\", weight, height, bats, throws, " +
+                        "debut, \"finalGame\", \"retroID\", \"bbrefID\", comment, startdate, enddate, \"year\") " +
+                        "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
                 PreparedStatement insertStatement = con.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
                 ResultSetMetaData metaData = rsEntry1.getMetaData();
-                for (int i = 1; i <= 46; i++) {
+                for (int i = 1; i <= 48; i++) {
                     if (mergedEntry[i] == null || mergedEntry[i].equals("null")) {
                         insertStatement.setNull(i, metaData.getColumnType(i + 1));
                     } else {
@@ -72,12 +78,15 @@ public class Duplicates {
                     newPk = generatedKeys.getInt(1);
                 }
                 if (newPk != -1) {
+                    updateOccupationSportsman(id1, id2, newPk, con);
+                    updateTeamSportsman(id1, id2, newPk, con);
+                    updateHallOfFame(id1, id2, newPk, con);
+                    updateCollegePlaying(id1, id2, newPk, con);
+                    updateRanking(id1, id2, newPk, con);
                     PreparedStatement deleteStatement = con.prepareStatement("DELETE FROM sportsman WHERE id = ? OR id = ?");
                     deleteStatement.setInt(1, id1);
                     deleteStatement.setInt(2, id2);
                     deleteStatement.executeUpdate();
-                    updateOccupationSportsman(id1, id2, newPk, con);
-                    updateTeamSportsman(id1, id2, newPk, con);
                 }
             }
         } catch (SQLException ex) {
@@ -112,6 +121,66 @@ public class Duplicates {
             }
         }
         String updateSql = "UPDATE occupation_sportsman set sportsman_id = ? WHERE sportsman_id = ? or sportsman_id = ?";
+        PreparedStatement updateSt = con.prepareStatement(updateSql);
+        updateSt.setInt(1, newPk);
+        updateSt.setInt(2, id1);
+        updateSt.setInt(3, id2);
+        updateSt.executeUpdate();
+    }
+
+
+    public static void updateHallOfFame(int id1, int id2, int newPk, Connection con) throws SQLException {
+        String sql_hall_of_fame = "SELECT sportsman_id, \"year\", voted_by FROM hall_of_fame WHERE sportsman_id = ? or sportsman_id = ?";
+        PreparedStatement stGet_hall_of_fame = con.prepareStatement(sql_hall_of_fame);
+        stGet_hall_of_fame.setInt(1, id1);
+        stGet_hall_of_fame.setInt(2, id2);
+        ResultSet rsGet_hall_of_fame = stGet_hall_of_fame.executeQuery();
+        while (rsGet_hall_of_fame.next()) {
+            String updateSQL = "UPDATE hall_of_fame SET sportsman_id = ? WHERE sportsman_id = ? and \"year\" = ? and voted_by = ?";
+            PreparedStatement updateSt = con.prepareStatement(updateSQL);
+            updateSt.setInt(1, newPk);
+            updateSt.setInt(2, rsGet_hall_of_fame.getInt("sportsman_id"));
+            updateSt.setInt(3, rsGet_hall_of_fame.getInt("year"));
+            updateSt.setString(4, rsGet_hall_of_fame.getString("voted_by"));
+            updateSt.executeUpdate();
+
+        }
+    }
+
+    public static void updateCollegePlaying(int id1, int id2, int newPk, Connection con) throws SQLException {
+        String sql_hall_of_fame = "SELECT sportsman_id, school_id, \"year\" FROM college_playing WHERE sportsman_id = ? or sportsman_id = ?";
+        PreparedStatement stGet_hall_of_fame = con.prepareStatement(sql_hall_of_fame);
+        stGet_hall_of_fame.setInt(1, id1);
+        stGet_hall_of_fame.setInt(2, id2);
+        ResultSet rsGet_hall_of_fame = stGet_hall_of_fame.executeQuery();
+        while (rsGet_hall_of_fame.next()) {
+            String updateSQL = "UPDATE college_playing SET sportsman_id = ? WHERE sportsman_id = ? and \"year\" = ? and school_id = ?";
+            PreparedStatement updateSt = con.prepareStatement(updateSQL);
+            updateSt.setInt(1, newPk);
+            updateSt.setInt(2, rsGet_hall_of_fame.getInt("sportsman_id"));
+            updateSt.setInt(3, rsGet_hall_of_fame.getInt("year"));
+            updateSt.setString(4, rsGet_hall_of_fame.getString("school_id"));
+            updateSt.executeUpdate();
+
+        }
+    }
+
+    public static void updateRanking(int id1, int id2, int newPk, Connection con) throws SQLException {
+        String sql_team_sportsman = "SELECT id, count(id) as c FROM ranking WHERE sportsman_id = ? or sportsman_id = ? GROUP BY id";
+        PreparedStatement stGet_team_sportsman = con.prepareStatement(sql_team_sportsman);
+        stGet_team_sportsman.setInt(1, id1);
+        stGet_team_sportsman.setInt(2, id2);
+        ResultSet rsGet_team_sportsman = stGet_team_sportsman.executeQuery();
+        while (rsGet_team_sportsman.next()) {
+            if (rsGet_team_sportsman.getInt("c") > 1) {
+                String deleteSql = "DELETE FROM ranking WHERE sportsman_id = ? and team_id = ?";
+                PreparedStatement deleteSt = con.prepareStatement(deleteSql);
+                deleteSt.setInt(1, id1);
+                deleteSt.setInt(2, rsGet_team_sportsman.getInt("id"));
+                deleteSt.executeUpdate();
+            }
+        }
+        String updateSql = "UPDATE ranking set sportsman_id = ? WHERE sportsman_id = ? or sportsman_id = ?";
         PreparedStatement updateSt = con.prepareStatement(updateSql);
         updateSt.setInt(1, newPk);
         updateSt.setInt(2, id1);
@@ -161,7 +230,7 @@ public class Duplicates {
         public String[] compareResultSets(ResultSet rs1, ResultSet rs2) throws SQLException {
             ArrayList<String> result = new ArrayList<String>();
             int columnCount = rs1.getMetaData().getColumnCount();
-            for (int i = 1; i <= columnCount - 1; i++) {
+            for (int i = 1; i <= columnCount; i++) {
                 result.add(this.compareString(rs1.getString(i), rs2.getString(i)));
             }
             return result.toArray(new String[result.size()]);
